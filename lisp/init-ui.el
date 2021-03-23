5296bb15f250c8ea181b5358b963390c24ee0aa5
qi.guo <arvin.qguo@gmail.com>
2021-03-24 01:56:40 +0800

    mod init-ui.el

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,18 +1,20 @@
-;; hide tool bar
-(tool-bar-mode -1)
-;; hide scroll bar
-(scroll-bar-mode -1)
+(if (display-graphic-p)
+    (progn
+      ;; hide tool-bar and scroll-bar
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)
 
-;; skip startup page
-(setq inhibit-splash-screen t)
+      ;; skip startup page
+      (setq inhibit-splash-screen t)
 
-;; cursor style
-(setq-default cursor-type 'bar)
+      ;; cursor style
+      (setq-default cursor-type 'bar)
 
-;; fullscreen startup
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
+      ;; fullscreen startup
+      (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-;; highlight current line
-(global-hl-line-mode t)
+      ;; highlight current line
+      (global-hl-line-mode t)
+      ))
 
 (provide 'init-ui)
