ceb8646f0c569184bbb449b94109b68838e3f4eb
qi.guo <qi.guo@shopee.com>
2021-03-25 10:33:58 +0800

    mod tidy init-keybindings.el and update .gitignore

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -1,20 +1,3 @@
-;; ivy swiper
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;; e(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
 ;; recentf mode
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
@@ -25,9 +8,6 @@
 
 ;; open init file
 (global-set-key (kbd "<f2>") 'open-init-file)
-
-;; find git dir files
-(global-set-key (kbd "C-c p g") 'counsel-git)
 
 ;; hippie complete
 (global-set-key (kbd "s-/") 'hippie-expand)
