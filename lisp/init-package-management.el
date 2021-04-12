001af97996568ddd5c2009d523e30317ffaba3bc
qi.guo <qi.guo@shopee.com>
2021-04-12 15:15:07 +0800

    mod config auto-package-update

--- lisp/init-package-management.el
+++ lisp/init-package-management.el
@@ -1,8 +1,10 @@
 ;; auto update packages
 (use-package auto-package-update
-  :custom
-  (auto-package-update-delete-old-versions t)
-  )
+   :ensure t
+   :config
+   ;; (setq auto-package-update-delete-old-versions t)
+   (setq auto-package-update-interval 4)
+   (auto-package-update-maybe))
 
 ;; ensure system package
 (use-package use-package-ensure-system-package)
