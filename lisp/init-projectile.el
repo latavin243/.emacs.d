af1ace8e520bec3f4084d589cb386805beb06e39
qi.guo <qi.guo@shopee.com>
2021-04-13 18:26:01 +0800

    mod add leader-pp to switch project

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -2,6 +2,9 @@
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (evil-leader/set-key
+    "pp" 'projectile-switch-project
+  )
   )
 
 (provide 'init-projectile)
