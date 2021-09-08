2941e1d80cb5c12808c988fb82d4dac7d672fb25
qi.guo <qi.guo@shopee.com>
2021-09-08 16:28:03 +0800

    mod use doom-theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,5 +1,25 @@
 (use-package srcery-theme)
+;; (use-package monokai-theme)
+;; (use-package gruvbox-theme)
 
-(load-theme 'srcery t)
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; (load-theme 'gruvbox-dark-hard t)
 
 (provide 'init-theme)
