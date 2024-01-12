9cf5d364a3bda841e06d3849bba1b49e5779850f
qi.guo <qi.guo@shopee.com>
2024-01-12 12:12:45 +0800

    mod update init.el, split more packages from init-packages

--- init.el
+++ init.el
@@ -6,38 +6,102 @@
 (add-to-list 'package-archives
              '("Melpa" . "https://melpa.org/packages/") t)
 
+;; * 0. package manager
+;; ** use-package
 (require 'use-package)
-
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-;; === load config files
-
-;; package manager
 (require 'init-elpa)
 
-;; basic
+;; * 1. basic
 (require 'init-defaults)
 (require 'init-basic)
 
-;; decoration
+;; * 2. decoration
+;; ** welcome page
+;; ** floatwindow
+;; ** bottom bar
+;; ** theme
 (require 'init-decoration)
 
-;; editing (vim)
-(require 'init-evil)
-
-;; navigation
+;; * 3. navigation
+;; ** file search
+;; ** code search
+;; ** sidebar file explorer
+;; ** project swtich
+;; ** window jump
+;; ** buffer / tab jump
+;; ** code jump
+;; ** file browser
 (require 'init-navigation)
 (require 'init-treemacs)
 (require 'init-swiper)
+(use-package window-numbering)
 
-;; coding (language, spell check, snippet)
+;; * 4. editing
+;; ** vim editing
+;; ** note taking (org, md, etc)
+;; ** narrow window
+;; ** format
+;; ** others
+(require 'init-evil)
+;; highlight TODO
+(use-package hl-todo
+  :ensure t
+  :defer t
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (defvar hl-todo-keyword-faces
+    `(("TODO"       warning bold)
+      ("FIXME"      error bold)
+      ("HACK"       font-lock-constant-face bold)
+      ("REVIEW"     font-lock-keyword-face bold)
+      ("NOTE"       success bold)
+      ("DEPRECATED" font-lock-doc-face bold))
+    )
+  )
+;; yasnippet config
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  )
+;; auto-yasnippet
+(use-package auto-yasnippet
+  :ensure t
+  :after (yasnippet)
+  :defer t
+  :config
+  (global-set-key (kbd "H-w") #'aya-create)
+  (global-set-key (kbd "H-y") #'aya-expand)
+  )
+
+;; undo and redo
+(use-package undo-tree
+  :ensure t
+  :config
+  (turn-on-undo-tree-mode)
+  ;; (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
+  )
+
+
+;; * 5. coding & language
+;; ** code syntax, completion
+;; ** spell check
+;; ** code format
+;; ** snippets
+;; ** playground
+;; ** runner
 (require 'init-language)
-
-;; dev tool (git, note)
-(require 'init-git)
 (require 'init-org)
 
-;; to be split
+;; * 6. dev tools
+;; ** git
+;; ** terminal
+;; ** copilot
+(require 'init-git)
+
+;; TODO
 ;; (require 'init-packages)
 
 ;; load custom.el
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -4,8 +4,6 @@
 ;;; Code:
 
 ;; important packages
-;; vim related
-(use-package window-numbering)
 ;; others
 (use-package exec-path-from-shell)
 (use-package reveal-in-osx-finder)
@@ -76,32 +74,6 @@
   (global-flycheck-mode t)
   )
 
-;; yasnippet config
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-reload-all)
-  (add-hook 'prog-mode-hook #'yas-minor-mode)
-  )
-
-;; auto-yasnippet
-(use-package auto-yasnippet
-  :ensure t
-  :after (yasnippet)
-  :defer t
-  :config
-  (global-set-key (kbd "H-w") #'aya-create)
-  (global-set-key (kbd "H-y") #'aya-expand)
-  )
-
-;; undo and redo
-(use-package undo-tree
-  :ensure t
-  :config
-  (turn-on-undo-tree-mode)
-  ;; (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
-  )
-
 ;; which key
 (use-package which-key
   :ensure t
@@ -115,9 +87,6 @@
   :custom
   (auto-package-update-delete-old-versions t)
   )
-
-
-
 
 ;; (use-package company-lsp
 ;;   :ensure t
@@ -135,24 +104,6 @@
 ;;   :hook ((after-init . nyan-mode))
 ;;   )
 
-
-
-
-;; highlight TODO
-(use-package hl-todo
-  :ensure t
-  :defer t
-  :hook (prog-mode . hl-todo-mode)
-  :config
-  (defvar hl-todo-keyword-faces
-    `(("TODO"       warning bold)
-      ("FIXME"      error bold)
-      ("HACK"       font-lock-constant-face bold)
-      ("REVIEW"     font-lock-keyword-face bold)
-      ("NOTE"       success bold)
-      ("DEPRECATED" font-lock-doc-face bold))
-    )
-  )
 
 ;; (use-package flyspell
 ;;   :ensure t
